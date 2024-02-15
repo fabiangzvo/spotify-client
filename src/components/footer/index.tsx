f7d133762eb2f5d72592785f2e79875347791d97
fabiangzvo <faguzman.97@gmail.com>
2024-02-15 15:11:38 -0500

    responsive footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,15 +2,17 @@ import SocialNetwork from "../socialNetwork";
 
 function Footer() {
   return (
-    <footer className="w-screen flex justify-between items-between pb-3 text-title px-10">
+    <footer className="w-screen flex justify-between items-between pb-3 text-title px-10 max-lg:px-5 max-lg:flex-col">
       <div className="flex flex-col pointer-events-none">
-        <span className="font-extrabold text-2xl max-md:text-4xl">
+        <span className="font-extrabold text-2xl max-lg:text-2xl">
           SpotiClient
         </span>
-        <p className="max-md:text-2xl">All rights reserved © 2023</p>
+        <p className="max-lg:text-lg text-paragraph">
+          All rights reserved © 2023
+        </p>
       </div>
-      <div className="flex font-bold items-center justify-center">
-        <span>Follow the creator:&emsp;</span>
+      <div className="flex items-center justify-center max-lg:my-2 max-lg:justify-between">
+        <span className="max-lg:text-lg">Follow the creator:&emsp;</span>
         <SocialNetwork />
       </div>
     </footer>
